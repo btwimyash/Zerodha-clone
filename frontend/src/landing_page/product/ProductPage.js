b749a0ff2019511d0dda9adad4762cbd913e58c5
Yash Gavali <yash16gavali@gmail.com>
2025-06-30 17:18:19 +0530

    Re-added frontend as a normal directory

---
+++ frontend/src/landing_page/product/ProductPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Universe from './Universe';
+import RightSection from './RightSection';
+import LeftSection from './LeftSection';
+
+function ProductPage() {
+    return ( 
+        <>
+        <Universe/>
+        <LeftSection/>
+        <RightSection/>
+        </>
+     );
+}
+
+export default ProductPage;
